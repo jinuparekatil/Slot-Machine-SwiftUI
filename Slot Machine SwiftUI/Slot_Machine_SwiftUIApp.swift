32429d8a7c4587e9a9423f7cdeeaf5943ac59e70
Jinu Joy <jinuparekatil@gmail.com>
2023-11-24 18:57:17 +0530

    Created new project name slot machine

---
+++ Slot Machine SwiftUI/Slot_Machine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_Machine_SwiftUIApp.swift
+//  Slot Machine SwiftUI
+//
+//  Created by Jinu on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Slot_Machine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
